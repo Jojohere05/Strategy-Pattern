25e8fd8110846b6e9a034271faf34cf0832f5ed2
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-05 20:40:46 +0530

    constructor defined

--- Duck.java
+++ Duck.java
@@ -2,7 +2,11 @@ public abstract class Duck {
     FlyBehaviour flyBehavior;
     QuackBehaviour quackBehavior;
     SwimBehaviour swimBehavior;
-
+    public Duck(FlyBehaviour flyBehavior, QuackBehaviour quackBehavior, SwimBehaviour swimBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+        this.swimBehavior = swimBehavior;
+    }
 
 
 }
