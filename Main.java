149a3e85082126bc74dab59ece00cf054fa22a42
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-05 20:52:44 +0530

    instance for specific duckclass

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Jhotika raja
+//23070126050
+// A2
+
+public class Main {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        Duck redhead = new RedheadDuck();
+        Duck rubber = new RubberDuck();
+        Duck decoy = new DecoyDuck();
+
+       
+    }
+}
